0d86d3f306f4cb65679fa5400d6fc4f2056fc2cb
Hüseyin ONUR <huseyin.onr60@gmail.com>
2025-08-25 14:40:54 +0300

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-@@ .. @@
--import type { NextConfig } from "next";
--
--const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com'],
+  },
 }
-+/** @type {import('next').NextConfig} */
-+const nextConfig = {
-   images: {
-   }
-}
+
+export default nextConfig
